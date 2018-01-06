193f4298d7e4803740eac788a4f76c3dced2a7cc
Assaf-Haik-Barouch <Assaf98@gmail.com>
2018-01-06 10:15:55 +0200

    addded hash table header file

---
+++ HashTable.h
@@ -0,0 +1,30 @@
+//
+// Created by Assaf-Haik-Barouch on 06/01/2018.
+//
+
+#ifndef DS_WET_4_HASHTABLE_H
+#define DS_WET_4_HASHTABLE_H
+
+#include "linkedList.h"
+
+
+template<typename T>
+class HashTable{
+private:
+    List<T>* table;
+    int size;
+
+public:
+    HashTable(int n):size(n), tabke(NULL){}
+
+
+
+
+
+};
+
+
+
+
+
+#endif //DS_WET_4_HASHTABLE_H
