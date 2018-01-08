15542f593e93c81cd07e983d288c295598077055
Assaf-Haik-Barouch <Assaf98@gmail.com>
2018-01-08 14:17:27 +0200

    second draft for testing script

--- runTests.sh
+++ runTests.sh
@@ -6,7 +6,7 @@ tests_len = $3
 
 for ((i=1; i<=tests_num; i++)); do
  mkdir "./test${i}"
- ./test.py "./test${i}/test"
+ ./test.py ${tests_len} "./test${i}/test"
  "./test${i}/test.in" > `./$prog` > "./test${i}/test.res"
 
  if diff "./test${i}/test.out" "./test${i}/test.res" >/dev/null ; then
