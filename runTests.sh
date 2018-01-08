342d03c62b0d382075b74f429209524cace61fda
Assaf-Haik-Barouch <Assaf98@gmail.com>
2018-01-08 14:57:48 +0200

    maybe finished?

--- runTests.sh
+++ runTests.sh
@@ -5,12 +5,12 @@ tests_num = $2
 tests_len = $3
 
 for ((i=1; i<=tests_num; i++)); do
- mkdir "./test${i}"
- ./test.py ${tests_len} "./test${i}/test"
- "./test${i}/test.in" > `./$prog` > "./test${i}/test.res"
+ mkdir "test$i"
+ ./test.py $tests_len "test${i}/test"
+ "./test$i/test.in" > eval "./$prog" > "./test$i/test.res"
 
- if diff "./test${i}/test.out" "./test${i}/test.res" >/dev/null ; then
-  rm -rf "./test${i}"
+ if diff "./test$i/test.out" "./test$i/test.res" >/dev/null ; then
+  rm -rf "./test$i"
  fi
 
 done
