5b0a065b11346b01076fc34c3bb5ef9489e51328
Assaf-Haik-Barouch <Assaf98@gmail.com>
2018-01-08 14:16:13 +0200

    initial draft for testing script

---
+++ runTests.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+prog = $1
+tests_num = $2
+tests_len = $3
+
+for ((i=1; i<=tests_num; i++)); do
+ mkdir "./test${i}"
+ ./test.py "./test${i}/test"
+ "./test${i}/test.in" > `./$prog` > "./test${i}/test.res"
+
+ if diff "./test${i}/test.out" "./test${i}/test.res" >/dev/null ; then
+  rm -rf "./test${i}"
+ fi
+
+done
